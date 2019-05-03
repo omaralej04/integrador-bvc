2838d0e5555915ebe8a78114543f8f1211c37c98
Omar Diaz <o_ma_rdiaz@hotmail.com>
2019-05-03 12:43:50 -0400

    Update project

--- src/app/ayuda/ayuda.page.ts
+++ src/app/ayuda/ayuda.page.ts
@@ -9,7 +9,108 @@ export class AyudaPage implements OnInit {
 
   constructor() { }
 
+  openModal(topic) {
+    let modal = this.modalCtrl.create(ModalTemplatePage, topic);
+    modal.present();
+  }
+
   ngOnInit() {
   }
 
+}
+
+// Modal Component
+@Component({
+  templateUrl: "modalTemplate.html"
+})
+
+export class ModalTemplatePage {
+  topico;
+
+  constructor(
+    public platform: Platform,
+    public params: NavParams,
+    public viewCtrl: ViewController,
+    public sanitizer: DomSanitizer
+  ) {
+
+    var topicos = [
+      {
+        name: 'Ingreso y Recuperación de Clave',
+        link: 'https://www.youtube.com/embed/pkMxzN7de9o',
+      },
+      {
+        name: 'Recuperación de Clave y Usuario',
+        link: 'https://www.youtube.com/embed/zeQ8KiICD04'
+      },
+      {
+        name: 'Asignación de Parámetros Personales',
+        link: 'https://www.youtube.com/embed/rq6dyJ49H-U',
+      },
+      {
+        name: 'Solicitud de Referencias Bancarias',
+        link: 'https://www.youtube.com/embed/XRP2rggKjjU',
+      },
+
+      {
+        name: 'Suspensión de Cheques',
+        link: 'https://www.youtube.com/embed/tNXodenHTFg&t=50s',
+      },
+
+      {
+        name: 'Solicitud y Activación de Chequeras',
+        link: 'https://www.youtube.com/embed/r06L7ENTfnE',
+      },
+
+      {
+        name: 'Solicitud de Cheques de Gerencia',
+        link: 'https://www.youtube.com/embed/_SfIRskgwAE',
+      },
+
+      {
+        name: 'Solicitud de Avance de Efectivo',
+        link: 'https://www.youtube.com/embed/8aeah4_cgPY',
+      },
+
+      {
+        name: 'Solicitud y Activación de Tarjetas de Débito',
+        link: 'https://www.youtube.com/embed/OjktWOzAvJ4',
+      },
+
+      {
+        name: 'Transferencias a Terceros',
+        link: 'https://www.youtube.com/embed/e-zeLIJtB9M',
+      },
+
+      {
+        name: 'Pago de Servicios',
+        link: 'https://www.youtube.com/embed/bCapVAb6ow4',
+      },
+
+      {
+        name: 'Pago de Tarjetas de Crédito',
+        link: 'https://www.youtube.com/embed/qTC9TNW2aDs',
+      },
+
+      {
+        name: 'Agenda de Pagos',
+        link: 'https://www.youtube.com/embed/GDeZvf4A3qI',
+      },
+
+      {
+        name: 'Domiciliación de Servicios',
+        link: 'https://www.youtube.com/embed/kXx1twGolB4',
+      },
+
+      {
+        name: 'Consulta y Solicitud de Fidecomiso',
+        link: 'https://www.youtube.com/embed/aLt2iLXadg4',
+      },
+    ]
+    this.topico = topicos[this.params.get('topic')]
+  }
+
+  dismiss() {
+    this.viewCtrl.dismiss();
+  }
 }
--- src/app/vol/vol.page.ts
+++ src/app/vol/vol.page.ts
@@ -12,4 +12,59 @@ export class VolPage implements OnInit {
   ngOnInit() {
   }
 
+  submitPOST() {
+    var login;
+    var pass;
+
+    login = this.loginDat;
+    pass = this.passDat;
+
+    console.log("Login: " + login + " Pass: " + pass)
+
+    //let header: {
+      //'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+      //'Content-Type': 'application/x-www-form-urlencoded',
+      //}
+
+      /*
+    let body = {
+      transactionRequest: '510',
+      key: "",
+      event: "",
+      sistemaOperativo: "",
+      browser: "",
+      versionCompleta: "",
+      datoLogin: login,
+      claveEntrada: pass
+    }; */
+    
+    /*
+    this.http.post<String>('/volRequest/VOL-Web/vc510.action;jsessionid=' + '06727472AD727BF5CFBD130BB239FBEF'
+    , body, {headers: header , responseType: 'html' as 'string'})
+    .subscribe(data => {
+      console.log(data);
+      var pageContent = data
+      var pageContentUrl = 'data:text/html;base64,' + btoa(pageContent as 'string');
+      this.openURL(pageContentUrl);
+    }); */
+
+    var pageContent = '<html><head></head><body><form id="loginForm" action="/volRequest/VOL-Web/vc510.action;jsessionid=06727472AD727BF5CFBD130BB239FBEF" method="post">' +
+    '<input type="hidden" name="datoLogin" value="' + login + '">' +
+    '<input type="hidden" name="claveEntrada" value="' + pass + '">' +
+    '<input type="hidden" name="transactionRequest" value="510" id="frmlogin_transactionRequest"/>' +
+    '<input type="hidden" name="key" value="" id="frmlogin_key"/>' +
+    '<input type="hidden" name="event" value="" id="frmlogin_event"/>' +
+    '<input type="hidden" name="sistemaOperativo" value="" id="frmlogin_sistemaOperativo"/>' +
+    '<input type="hidden" name="browser" value="" id="frmlogin_browser"/>' +
+    '<input type="hidden"  id="versionCompleta" name="versionCompleta" />' +
+    '</form> <script type="text/javascript">document.getElementById("loginForm").submit() />'
+
+
+    var pageContentUrl = 'data:text/html;base64,' + btoa(pageContent as 'string');
+    this.openURL(pageContentUrl);
+  }
+
+  public openURL(url) {
+    this.oup.openURL(url);
+  }
 }
